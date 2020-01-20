a9692e29a4bf0ed5f0d2167a557050a79c9c5fee
Aidan Rocke <aidanrocke@gmail.com>
2020-01-20 20:06:56 +0000

    add partial derivative

--- cauchy_diff.jl
+++ cauchy_diff.jl
@@ -16,3 +16,26 @@ function nabla(f, x::Float64, delta::Float64)
   return expectation
 
 end
+
+q(X) = X[1] + X[2]^2 + cos(X[3])
+
+q2 = x -> q(x*kd(2,3) .+ X.*(ones(3)-kd(2,3)))
+
+q3 = x -> q(x*kd(3,3) .+ X.*(ones(3)-kd(3,3)))
+
+function partial_nabla(f, i::Int64, X::Float64,delta::Float64)
+
+  ## f:= the function to be differentiated
+  ## i:= partial differentiation with respect to this index
+  ## X:= where the partial derivative is evaluated
+  ## delta:= the sampling frequency
+
+  N = len(X)
+
+  kd(i,n) = [j==i for j in 1:n]
+
+  f_x = x -> f(x*kd(i,N) .+ X.*(ones(N)-kd(i,N)))
+
+  return nabla(f_x,X[i],delta)
+
+end
